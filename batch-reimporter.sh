4d489b5c9af7399b5acad4e336550071f9b73ccf
Emmanuel Otton <esup@mines-albi.fr>
2016-02-16 08:39:45 +0100

    tabgroups ok sur 4.0.15

---
+++ batch-reimporter.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#./reimporter.sh exports/fragment-definition/Academics.fragment-definition.xml
+./reimporter.sh exports/fragment-definition/Admin.fragment-definition.xml
+./reimporter.sh exports/fragment-definition/Campus.fragment-definition.xml
+./reimporter.sh exports/fragment-definition/Developer.fragment-definition.xml
+./reimporter.sh exports/fragment-definition/Staff.fragment-definition.xml
+./reimporter.sh exports/fragment-definition/permanents.fragment-definition.xml
